756d77d92bae9068c30a29ac00f172e31ea988f9
Milos Ojdanic <milosmne63@gmail.com>
2018-10-22 16:36:43 +0200

    .net Component - parser

---
+++ ComponentRepository/netComponent/netComponent/ExecutableProjectTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using netComponent;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecutableProjectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IComponentParser parser = new DotNetParser();
+            byte[] dllFile = File.ReadAllBytes("C:\\Users\\milos\\OneDrive\\Desktop\\Mr MDH\\ComponentTechnologies\\Project\\ComponentRepository\\netComponent\\netComponent\\netComponent\\bin\\Debug\\netComponent.dll");
+            Console.WriteLine(parser.parseComponentFile(dllFile));
+        }
+    }
+}
---
+++ ComponentRepository/netComponent/netComponent/netComponent/DotNetParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netComponent
+{
+    public class DotNetParser : IComponentParser
+    {
+        public string parseComponentFile(byte[] file)
+        {
+            Assembly assembly = Assembly.ReflectionOnlyLoad(file);
+            Console.WriteLine(assembly.ToString());
+            String classes = "{ 'classes' : [";
+            String interfaces = "'interfaces' : [";
+
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (type.IsClass)
+                {
+                    classes += type.Name + " , ";
+                }
+                else if (type.IsInterface)
+                {
+                    interfaces += type.Name + " , ";
+                }
+            }
+            classes = classes.Substring(0, classes.Length - 3) + "],";
+            interfaces = interfaces.Substring(0, interfaces.Length - 3) + "]}";
+            string jsonTemplate = classes + interfaces;
+            return jsonTemplate;
+        }
+
+    }
+}
